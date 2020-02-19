16b1c85ef5dbf6da17a89132f1b17bdc6357a1cc
wenyu0 <371290102@qq.com>
2020-02-19 19:55:57 +0800

    商品模块的整合

--- src/main/java/hust/shixun/grouptravel/adminManagement/controller/AdminController.java
+++ src/main/java/hust/shixun/grouptravel/adminManagement/controller/AdminController.java
@@ -2,6 +2,8 @@ package hust.shixun.grouptravel.adminManagement.controller;
 
 import hust.shixun.grouptravel.adminManagement.entities.Admin;
 import hust.shixun.grouptravel.adminManagement.service.AdminService;
+import hust.shixun.grouptravel.entities.Order;
+import hust.shixun.grouptravel.orderManagerment.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
---
+++ src/main/java/hust/shixun/grouptravel/itemsManagement/serviceImp/ProductServiceImp.java
@@ -0,0 +1,103 @@
+package hust.shixun.grouptravel.itemsManagement.serviceImp;
+
+import hust.shixun.grouptravel.entities.Notes;
+import hust.shixun.grouptravel.entities.Product;
+import hust.shixun.grouptravel.itemsManagement.mapper.ProductMapper;
+import hust.shixun.grouptravel.itemsManagement.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ProductServiceImp implements ProductService {
+    @Autowired
+    public ProductMapper productMapper;
+
+
+    @Override
+    public List<Product> queryAllProducts(){
+        List<Product> products = productMapper.queryAllProducts();
+        return products;
+    }
+
+    @Override
+    public Product queryProductById(Integer id){
+        Product product = productMapper.queryProductById(id);
+        return product;
+    }
+
+    @Override
+    public List<Product> queryProductByName(String name)  {
+        List<Product> products = productMapper.queryProductByName(name);
+        return products;
+    }
+
+    @Override
+    public Boolean addProduct(Product product){
+        int i=0;
+        try {
+            i = productMapper.addProduct(product);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        if (i>0)
+            return true;
+        else
+            return false;
+
+    }
+
+
+    @Override
+    public Boolean updateProduct(Product product){
+        int i=0;
+        try {
+            i = productMapper.updateProduct(product);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        if (i>0)
+            return true;
+        else
+            return false;
+
+    }
+
+
+    @Override
+    public Boolean deleteProductById(int id){
+        int i=0;
+        try {
+            i = productMapper.deleteProductById(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        if (i>0)
+            return true;
+        else
+            return false;
+
+    }
+
+    @Override
+    public List<Integer> queryHotProductId(Map<String, String> map) {
+        System.out.println(map);
+        map.size();
+        List<Integer> list=productMapper.queryHotProductId(map);
+        System.out.println(list);
+        return list;
+    }
+
+    @Override
+    public List<Notes> queryNoteByProductId (int id) {
+        return productMapper.queryNoteByProductId (id);
+    }
+
+
+}
