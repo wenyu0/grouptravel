da630e30a8afb53ad73e4044999936e6da889cf0
wenyu0 <371290102@qq.com>
2020-02-22 11:30:29 +0800

    所有功能模块整合完毕

---
+++ src/main/java/hust/shixun/grouptravel/userManagement/controller/UserController.java
@@ -0,0 +1,114 @@
+package hust.shixun.grouptravel.userManagement.controller;
+
+import hust.shixun.grouptravel.entities.Notes;
+import hust.shixun.grouptravel.entities.Order;
+import hust.shixun.grouptravel.entities.Product;
+import hust.shixun.grouptravel.userManagement.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+@Controller
+public class UserController {
+    @Autowired
+    public UserService userService;
+
+    @RequestMapping("/user/addOrder")
+    @ResponseBody
+    public boolean addOrder(Order order) {
+        return userService.addOrder(order);
+    }
+
+    @RequestMapping("/user/queryProductById")
+    @ResponseBody
+    public Product queryProductById(@RequestParam(value = "productId") int productId) {
+        return userService.queryProductById(productId);
+    }
+
+    @RequestMapping("/user/queryProductByName")
+    @ResponseBody
+    public List<Product> queryProductByName(@RequestParam(value = "productName") String productName) {
+        return userService.queryProductByName(productName);
+    }
+
+    @RequestMapping("/user/queryProductByProvince")
+    @ResponseBody
+    public List<Product> queryProductByProvince(@RequestParam(value = "provinceName") String provinceName) {
+        List<Product> products = new ArrayList<>();
+        List<Integer> cityList = userService.getCityList(provinceName);
+        for(int cityId:cityList)
+            products.addAll(userService.queryProductByCity(cityId));
+        return products;
+    }
+
+    @RequestMapping("/user/queryOrdersWithUnpay")
+    @ResponseBody
+    public List<Order> queryOrdersWithUnpay(@RequestParam(value = "userId") int userId) {
+        return userService.queryOrdersWithUnpay(userId);
+    }
+
+    @RequestMapping("/user/updateUnpayOrder")
+    @ResponseBody
+    public Boolean updateUnpayOrder(@RequestParam(value = "orderId") int orderId) {
+        return userService.updateUnpayOrder(orderId);
+    }
+
+    @RequestMapping("/user/queryNotesByOrderId")
+    @ResponseBody
+    public List<Notes> queryNotesByOrderId(int orderId) {
+        return userService.queryNotesByOrderId(orderId);
+    }
+
+    @RequestMapping("/user/addNotes")
+    @ResponseBody
+    public Boolean addNotes(Notes notes) {
+        return userService.addNotes(notes);
+    }
+
+    @RequestMapping("/user/queryOrderPrice")
+    @ResponseBody
+    public double queryOrderPrice(@RequestParam(value = "orderId") int orderId) {
+        return userService.queryOrderPrice(orderId);
+    }
+
+    @RequestMapping("/user/updateOrderPrice")
+    @ResponseBody
+    public Boolean updateOrderPrice(@RequestParam(value = "orderId") int orderId,
+                                    @RequestParam(value = "orderPrice") double orderPrice) {
+        return userService.updateOrderPrice(orderId,orderPrice);
+    }
+
+    @RequestMapping("/user/queryOrder")
+    @ResponseBody
+    public List<Order> queryOrder(@RequestParam(value = "orderId") int orderId) {
+        return userService.queryOrder(orderId);
+    }
+
+    @RequestMapping("/user/addRates")
+    @ResponseBody
+    public Boolean addRates(double rate, int orderId){
+        return  userService.addRates(rate,orderId);
+    }
+
+    @RequestMapping("/user/likeNotes")
+    @ResponseBody
+    public Boolean likeNotes(int userId,int notesId){
+        return userService.likeNotes(userId,notesId);
+    }
+
+    @RequestMapping("/user/commentNotes")
+    @ResponseBody
+    public Boolean commentNotes(int userId,int notesId,String commentContent){
+        return userService.commentNotes(userId,notesId,commentContent);
+    }
+
+    @RequestMapping("/user/queryLikeNotes")
+    @ResponseBody
+    public List<Notes> queryLikeNotes(int userId){
+        return userService.queryLikeNotes(userId);
+    }
+}
---
+++ src/main/java/hust/shixun/grouptravel/userManagement/mapper/UserMapper.java
@@ -0,0 +1,83 @@
+package hust.shixun.grouptravel.userManagement.mapper;
+
+import hust.shixun.grouptravel.entities.Notes;
+import hust.shixun.grouptravel.entities.Order;
+import hust.shixun.grouptravel.entities.Product;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+    //传入市的id得到省的id
+    @Select("select provinceId from gt_city where cityId = #{cityId}")
+    int getProvinceId(int cityId);
+
+    //通过省的名字得到市的集合
+    @Select("SELECT cityId from gt_city where provinceId in(SELECT provinceId from gt_province where provinceName = #{provinceName})")
+    List<Integer> getCityList(String provinceName);
+
+    @Insert("INSERT into gt_order(createTime,productId,userId,orderPrice,payTime,status,PTid,pNum,currentDiscount,travelTime,notesId)" +
+            " VALUES(#{createTime},#{productId},#{userId},#{orderPrice},#{payTime},#{status},#{PTid},#{pNum},#{currentDiscount},#{travelTime},#{notesId})")
+    boolean addOrder(Order order);
+
+    //通过旅游产品id查询旅游产品，并返回该旅游产品。
+    @Select("SELECT * FROM gt_product where productId=#{productId}")
+    Product queryProductById(int productId);
+
+    //通过旅游产品name查询旅游产品，并返回该旅游产品。
+    @Select("SELECT * FROM gt_product where productName like '%#{productName}%'")
+    List<Product> queryProductByName(String productName);
+
+    //通过旅游产品所在地cityId查询旅游产品，并返回属于同一个市的所有旅游产品的集合。
+    @Select("SELECT * FROM gt_product WHERE cityId = #{cityId}")
+    List<Product> queryProductByCity(int cityId);
+
+    //通过所有未被支付的订单，并返回所有未被支付的订单的集合
+    @Select("SELECT * FROM gt_order WHERE status=0 AND userId=#{userId}")
+    List<Order> queryOrdersWithUnpay(int userId);
+
+    //更新未被支付的订单，如更新成功则返回ture否则为false。(用户付款将状态更新为已支付)
+    @Select("UPDATE gt_order SET status=1 WHERE orderId=#{orderId}")
+    Boolean updateUnpayOrder(int orderId);
+
+    //通过名字查询相应的旅游项目中的所有已完成的订单号（唯一主键），通过此查询其游记评论表中的信息并返回其一个集合。
+    @Select("SELECT * FROM gt_notes,gt_order WHERE gt_notes.notesId=gt_order.notesId AND gt_order.orderId=#{orderId}")
+    List<Notes> queryNotesByOrderId(int orderId);
+
+    //添加游记
+    @Insert("INSERT INTO gt_notes(title,content,writeTime,rate,productId) VALUES(#{title},#{content},#{writeTime},#{rate},#{productId})")
+    Boolean addNotes(Notes notes);
+
+
+    //查询当前订单的价格
+    @Select("SELECT orderPrice FROM gt_order WHERE orderId = #{orderId}")
+    double queryOrderPrice(int orderId);
+
+    //修改与该订单相关联的（由于拼团）所有订单的价格
+    @Update("UPDATE gt_order SET orderPrice = #{orderPrice} WHERE PTid in (SELECT PTid from(SELECT PTid from gt_order WHERE orderId = #{orderId})as A) ")
+    Boolean updateOrderPrice(int orderId, double orderPrice);
+
+    //查询与当前订单所关联的所有订单
+    @Select("Select * from gt_order WHERE PTid in (SELECT PTid from gt_order WHERE orderId = #{orderId})")
+    List<Order> queryOrder(int orderId);
+
+    //对产品评分
+    @Update("UPDATE gt_notes set rate = #{rate} where notesId in(SELECT notesId from gt_order where orderId = #{orderId})")
+    Boolean addRates(double rate,int orderId);
+
+    //点赞游记
+    @Insert("INSERT INTO gt_noteslike(userId,notesId) values(#{userId},#{notesId})")
+    Boolean likeNotes(int userId,int notesId);
+
+    //评论游记
+    @Insert("INSERT INTO gt_notescomment(userId,notesId,commentContent) VALUES(#{userId},#{notesId},#{commentContent})")
+    Boolean commentNotes(int userId,int notesId,String commentContent);
+
+    //查看点赞的游记
+    @Select("SELECT * from gt_notes where notesId in (SELECT notesId from gt_noteslike where userId = #{userId})")
+    List<Notes> queryLikeNotes(int userId);
+}
